a59b321790379526146f10e09555dcd33f59b7db
EWRC01 <edwin1026@hotmail.es>
2024-02-14 10:20:59 -0600

    Add Update Function

--- sqlite_project/src/product/product.service.ts
+++ sqlite_project/src/product/product.service.ts
@@ -6,51 +6,62 @@ import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductService {
-
-
   constructor(
     @InjectModel(Product)
     private productRepository: typeof Product,
   ) {}
 
-  create(createProductDto: CreateProductDto) {
-    return  this.productRepository.create(createProductDto as any)
+  async create(createProductDto: CreateProductDto) {
+    return await this.productRepository.create(createProductDto as any);
   }
 
-  findAll() {
-    return this.productRepository.findAll();
+  async findAll() {
+    return await this.productRepository.findAll();
   }
 
   async findOne(id: number) {
-
-    const productFound = await this.productRepository.findOne(  {where: {
-      id
-    }})
+    const productFound = await this.productRepository.findOne({
+      where: {
+        id,
+      },
+    });
 
     if (!productFound) {
-      return new HttpException('User not Found', HttpStatus.NOT_FOUND)
+      return new HttpException('User not Found', HttpStatus.NOT_FOUND);
     }
-    return productFound
+    return productFound;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const productFound = await this.productRepository.findOne({
+      where: { id },
+    });
 
+    if (!productFound) {
+      throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
+    }
+
+    await this.productRepository.update(updateProductDto, { where: { id } });
+
+    const updatedProduct = await this.productRepository.findByPk(id);
+    return updatedProduct;
   }
 
   async remove(id: number) {
-    
-    const productFound = await this.productRepository.findOne(  {where: {
-      id
-    }})
+    const productFound = await this.productRepository.findOne({
+      where: {
+        id,
+      },
+    });
 
-    if(!productFound) {
+    if (!productFound) {
       return new HttpException('Product not Found', HttpStatus.NOT_FOUND);
     }
-    
+
     return this.productRepository.destroy({
       where: {
-        id
-      }
-    })
+        id,
+      },
+    });
   }
 }
